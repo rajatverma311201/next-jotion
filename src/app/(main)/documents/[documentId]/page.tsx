6e5dc47fd92a96779e00666360706b4c97f69f89
Rajat Verma <rajat311201@gmail.com>
2023-11-12 04:41:36 +0530

    added toolbar

--- src/app/(main)/documents/[documentId]/page.tsx
+++ src/app/(main)/documents/[documentId]/page.tsx
@@ -1,10 +1,31 @@
+"use client";
+import { Toolbar } from "@/components/toolbar";
+import { useQuery } from "convex/react";
+import { api } from "@/../convex/_generated/api";
+import { Id } from "@/../convex/_generated/dataModel";
+
 interface DocumentIdPageProps {
     params: {
-        documentId: string;
+        documentId: Id<"documents">;
     };
 }
 const DocumentIdPage: React.FC<DocumentIdPageProps> = ({ params }) => {
-    return <div>DocumentIdPage - {JSON.stringify(params)}</div>;
+    const document = useQuery(api.documents.getById, {
+        documentId: params.documentId,
+    });
+
+    return (
+        <>
+            <div>DocumentIdPage - {JSON.stringify(params)}</div>
+            <div className="mx-auto mt-5 md:max-w-3xl lg:max-w-4xl">
+                {document && <Toolbar initialData={document!} />}
+                {/* <Editor
+          onChange={onChange}
+          initialContent={document.content}
+        /> */}
+            </div>
+        </>
+    );
 };
 
 export default DocumentIdPage;
